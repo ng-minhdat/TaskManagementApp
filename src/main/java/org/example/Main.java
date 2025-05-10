05a838ab07646bb82d5fca1eaa5908c734228314
ng-minhdat <nmdat21@clc.fitus.edu.vn>
2025-05-10 19:28:28 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import service.CommentService;
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        CommentService service = new CommentService();
+    }
+}
---
+++ src/main/java/service/CommentService.java
@@ -0,0 +1,8 @@
+package service;
+
+public class CommentService {
+
+    public CommentService() {
+        System.out.println("CommentService object created");
+    }
+}
